1204b3a250f51f441e7d8dec04856e30a1519067
Justin C <jdcblades@gmail.com>
2016-05-25 20:05:16 -0700

    updated bot

--- BirthdayBuddy.js
+++ BirthdayBuddy.js
@@ -1,7 +1,6 @@
 const TwitterBot = require("node-twitterbot").TwitterBot;
 const config = require("./config");
 
-// Include your access information below
 var Bot = new TwitterBot(config);
 
 Bot.tweet("@BabbyCanada we meet again");
